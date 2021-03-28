b022978db78fc4ddd6aee16f3e6192e7183b6b81
Marco Marinho <marcomarinho12@gmail.com>
2021-03-28 15:23:33 +0100

    update blobs

--- oneplus5.mk
+++ oneplus5.mk
@@ -193,10 +193,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
-    libspl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.3-service.clearkey
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
